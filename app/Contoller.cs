395999306a9d9960ab82e06b4618158cbdd5dbc5
Luke Storry <Luke@storry.uk>
2021-03-22 17:40:03 +0000

    working app

---
+++ app/Contoller.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+
+[ApiController]
+[Route("/")]
+public class Controller : ControllerBase
+{
+    private readonly ILogger<Controller> _logger;
+    private readonly IConfiguration _configuration;
+
+    public Controller(ILogger<Controller> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public OutputData Get()
+    {
+        var deploymentMethod = _configuration["DEPLOYMENT_METHOD"] ?? "Unknown";
+        using (var connection = new SqlConnection(_configuration["ConnectionString"]))
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                return new OutputData
+                {
+                    Status = $"Couldn't open db connection: {e.Message}",
+                    DeploymentMethod = deploymentMethod
+                };
+            }
+            using (SqlCommand command = new SqlCommand("select count(*) from SalesLT.Product", connection))
+            {
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        return new OutputData
+                        {
+                            Status = $"Connected to db, {reader.GetInt32(0)} rows found",
+                            DeploymentMethod = _configuration["DEPLOYMENT_METHOD"] ?? "Unknown"
+                        };
+                    }
+                }
+                catch (Exception e)
+                {
+                    return new OutputData
+                    {
+                        Status = $"Connected to DB but no data found: {e.Message}",
+                        DeploymentMethod = _configuration["DEPLOYMENT_METHOD"] ?? "Unknown"
+                    };
+                }
+            }
+
+        }
+    }
+    public class OutputData
+    {
+        public string CurrentDate => $"{DateTime.Now:f}";
+
+        public string Status { get; set; }
+        public string DeploymentMethod { get; set; }
+    }
+}
--- app/Startup.cs
+++ app/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,13 +7,6 @@ namespace app
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -34,3 +26,4 @@ namespace app
         }
     }
 }
+
