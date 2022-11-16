7106224a1885051eb65f0776dc43be75feb705df
Alex Jones <alexander.jones@softwire.com>
2022-11-16 16:32:24 +0000

    Bump azurerm provider version

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+data "azurerm_linux_web_app" "example" {
+  name                = "to-do-app-kl"
+  resource_group_name = "LV21_KeithLeverton_ProjectExercise"
+}
+
